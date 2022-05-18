6ceb003060de1a1955b88f12934c7cd40c848a15
James Wu <justinwpro@gmail.com>
2022-05-17 23:13:30 -0500

    update

---
+++ basic_javascript/practice_1.js
@@ -0,0 +1,10 @@
+console.log(typeof true);
+
+let abc;
+console.log(typeof abc);
+
+console.log(typeof null); //------------------------------------------------------------
+
+const name= "James";
+console.log("my name is" +" "+ name);
+console.log(`my name is ${name}`);
---
+++ basic_javascript/practice_2.js
@@ -0,0 +1,10 @@
+let a = 11;
+console.log(String(a));
+
+let b = "11";
+console.log(Number(b));
+
+console.log(11 + "11");
+console.log(11-"11");
+
+console.log("11"-"10" + 2 + "3");
