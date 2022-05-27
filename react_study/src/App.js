831750ec381ef99b472da84b1db5ccbe6e19df09
James Wu <justinwpro@gmail.com>
2022-05-26 23:27:21 -0500

    update

--- react_study/src/App.js
+++ react_study/src/App.js
@@ -4,25 +4,14 @@ import { Data } from "./data/Data.js";
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return (
-    <>
-      <ExpenseItem
-        time={Data[0].time}
-        name={Data[0].name}
-        price={Data[0].price}
-      />
-      <ExpenseItem
-        time={Data[1].time}
-        name={Data[1].name}
-        price={Data[1].price}
-      />
-      <ExpenseItem
-        time={Data[2].time}
-        name={Data[2].name}
-        price={Data[2].price}
-      />
-    </>
-  );
+  return Data.map((oneData) => {
+    const { time, name, price } = oneData;
+    return (
+      <>
+        <ExpenseItem time={time} name={name} price={price} />
+      </>
+    );
+  });
 }
 
 export default App;
