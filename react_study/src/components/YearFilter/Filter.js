94ea51268f46e42e36a0bfe01a48c5a4efea0ddd
James Wu <justinwpro@gmail.com>
2022-05-29 00:11:10 -0500

    update

--- react_study/src/components/YearFilter/Filter.js
+++ react_study/src/components/YearFilter/Filter.js
@@ -5,17 +5,19 @@ import Expense from "../Expenses/Expense";
 const Filter = (props) => {
   const { Data } = props;
   return (
-    <>
-      <div>Filter by year</div>
-      <div>
-        <select>
-          <option>2020</option>
-          <option>2021</option>
-          <option>2022</option>
-        </select>
+    <div className="filter">
+      <div className="two">
+        <div className="content">Filter by year</div>
+        <div className="year">
+          <select>
+            <option>2020</option>
+            <option>2021</option>
+            <option>2022</option>
+          </select>
+        </div>
       </div>
       <Expense Data={Data} />
-    </>
+    </div>
   );
 };
 
