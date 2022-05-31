8b1a47b7d165a27e504d4bc1d04594856ffc8d18
James Wu <justinwpro@gmail.com>
2022-05-31 11:21:00 -0500

    update

--- react_study_3/src/component/Input.js
+++ react_study_3/src/component/Input.js
@@ -1,17 +1,38 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef,useEffect } from "react";
 import "./Input.css";
 
 const Input = (props) => {
   const [valueOne, setValueOne] = useState("");
   const inputSecondHandler = useRef();
+
   const inputOneHandler = (e) => {
     setValueOne(e.target.value);
   };
 
+  const [valid, setValid] = useState(true);
+
+
+  useEffect(() => {
+    const timing = setTimeout(() => {
+      setValid(valueOne.length > 0);
+      console.log("try");
+    },200);
+    return () => {
+      clearTimeout(timing);
+    }
+    
+  },[valueOne]);
+
+
   const submitData = (e) => {
     e.preventDefault();
-    props.getData(valueOne);
-    props.getData(inputSecondHandler.current.value);
+    if(valid === false){
+      console.log("input wrong");
+    }else{
+      props.getData(valueOne);
+      props.getData(inputSecondHandler.current.value);
+    }
+    
   };
 
   return (
@@ -20,11 +41,11 @@ const Input = (props) => {
         <form className="form" onSubmit={submitData}>
           <div>
             <label htmlFor="input_1">input_1</label>
-            <input id="input_1" onChange={inputOneHandler} value={valueOne} />
+            <input id="input_1" onChange={inputOneHandler} value={valueOne} className={valid ? "input_valid" : "input_notvalid"}/>
           </div>
           <div>
             <label htmlFor="input_2">input_2</label>
-            <input id="input_2" ref={inputSecondHandler} />
+            <input id="input_2" ref={inputSecondHandler} className={valid ? "input_valid" : "input_notvalid"}/>
           </div>
           <div>
             <button className="button">press it!</button>
