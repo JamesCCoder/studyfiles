951c51528206649f73422dbb37ffbbba6de3c061
James Wu <justinwpro@gmail.com>
2022-06-01 23:15:19 -0500

    update

--- react_study_10/src/Components/Fetch.js
+++ react_study_10/src/Components/Fetch.js
@@ -5,30 +5,45 @@ const Fetch = (props) => {
   const { getData } = props;
   const [listData, setListData] = useState([{}]);
   let [N, setN] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
+
   const fetchHandler = async () => {
-    const data = await fetch(`https://swapi.dev/api/films/${N}`);
-    const data_1 = await data.json();
-    setListData((pre) => {
-      return [
-        ...pre,
-        {
-          id: Math.random(),
-          title: data_1.title,
-          content: data_1.opening_crawl,
-        },
-      ];
-    });
-    getData(listData);
-    console.log(listData);
-    setN((pre) => {
-      return pre + 1;
-    });
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(`https://swapi.dev/api/films/${N}`);
+      if (!response.ok) {
+        throw new Error("something went wrong!");
+      }
+      const data_1 = await response.json();
+
+      setListData((pre) => {
+        return [
+          ...pre,
+          {
+            id: Math.random(),
+            title: data_1.title,
+            content: data_1.opening_crawl,
+          },
+        ];
+      });
+      getData(listData);
+      setN((pre) => {
+        return pre + 1;
+      });
+      setLoading(false);
+    } catch (error) {
+      setError(error.message);
+    }
   };
 
   return (
     <div className="fetch_container">
       <button className="fetch_button" onClick={fetchHandler}>
-        Fetch Movies
+        {!loading && "Fetch Movies"}
+        {loading && "Loading..."}
+        {loading && error && `${error}`}
       </button>
     </div>
   );
