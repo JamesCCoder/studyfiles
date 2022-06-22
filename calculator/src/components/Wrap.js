80a227d1e10b777975b4554a5575632d1132c68c
James Wu <justinwpro@gmail.com>
2022-06-22 11:43:31 -0500

    update

---
+++ calculator/src/components/Wrap.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Wrap.css";
+
+const Wrap = ({ children }) => {
+  return <div className="wrap">{children}</div>;
+};
+
+export default Wrap;
---
+++ test1.js
@@ -0,0 +1,12 @@
+function func1() {
+  return {
+    name: "Akarshan",
+  };
+}
+function func2() {
+  return {
+    name: "Akarshan",
+  };
+}
+console.log(func1());
+console.log(func2());
