57b1b6112238b2de5921ec2b0a90a778d2804dbc
James Wu <justinwpro@gmail.com>
2022-06-28 23:48:09 -0500

    updte

---
+++ React_challenge/addchildren/src/component/children.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Children = () => {
+  return <p>This is children</p>;
+};
+
+export default Children;
---
+++ React_challenge/addchildren/src/component/parent.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Parent = ({ children }) => {
+  return (
+    <>
+      <p>124</p>
+      <div>{children}</div>
+    </>
+  );
+};
+
+export default Parent;
