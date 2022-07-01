5321ab43ccf10dde589589875ac3e33601e1d247
James Wu <justinwpro@gmail.com>
2022-06-30 20:07:05 -0500

    update

---
+++ React_challenge/picture/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+import logo from "./logo.png";
+function App() {
+  return (
+    <div>
+      <img src={logo} alt="logo" />
+    </div>
+  );
+}
+
+export default App;
