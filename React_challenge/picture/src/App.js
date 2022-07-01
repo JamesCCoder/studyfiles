d47c75433c79c928588f397168c727f81cf1cbcf
James Wu <justinwpro@gmail.com>
2022-06-30 22:03:13 -0500

    update

--- React_challenge/picture/src/App.js
+++ React_challenge/picture/src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.css";
 import logo from "./logo.png";
+
+window.$name = "123";
 function App() {
+  const name = window.$name;
   return (
     <div>
       <img src={logo} alt="logo" />
+      <p>{React.version}</p>
+      <p>{name}</p>
     </div>
   );
 }
