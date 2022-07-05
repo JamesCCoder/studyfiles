310acb7b8953930510098f85bf071a50c791089e
James Wu <justinwpro@gmail.com>
2022-07-05 10:17:43 -0500

    update

--- React_challenge/advanced-todolist/src/App.js
+++ React_challenge/advanced-todolist/src/App.js
@@ -7,11 +7,13 @@ function App() {
   let [searchValue, setSearchValue] = useState("");
   let [item, setItem] = useState([]);
   let [order, setOrder] = useState(1);
+  let [text, setText] = useState(false);
   const addHandler = () =>{
       let newItem = {
         id: Math.random(),
         content: value,
         order:order,
+        text:text,
       }
     setItem((pre) => [...pre, newItem]);
     setValue("");
@@ -22,15 +24,35 @@ function App() {
      })
      setItem(newItem);
   }
+
+  const changeOrder = (abc, order) =>{
+    let currentItem = item.filter((one) => abc.id === one.id);
+    let newItem1 = {
+      id: abc.id,
+      content: abc.content,
+      order: Number(order),
+      text:abc.text,
+    };
+    deleteHandler(abc.id);
+    setItem((pre) => [...pre, newItem1]);
+  }
+
+  const textClick = () =>{
+    setText(!text);
+  }
   return (
     <div>
        {item.length !== 0 && <input type="text" placeholder="search the to do list" value={searchValue} onChange={(e) => setSearchValue(e.target.value)}/>}
        <ul>
        {
-         item.filter(li => li.content.toLowerCase().includes(searchValue.toLowerCase()))
+         item.filter(li => li.content.includes(searchValue))
+         .sort( (a,b) => a.order - b.order)
          .map((one) => {
            return (
-               <li key={one.id}><input type="number" value={order} onChange={(e) => setOrder(e.target.value)}/>{one.content}
+               <li key={one.id}><input type="number" 
+               value={one.order} 
+               onChange={(e) => {changeOrder(one, e.target.value)}}/>
+               <span style={{textDecorationLine:"line-through"}} onClick={() => setText()}>{one.content}</span>
                <button>edit</button>
                <button onClick={() => deleteHandler(one.id)}>delete</button>
                </li>
