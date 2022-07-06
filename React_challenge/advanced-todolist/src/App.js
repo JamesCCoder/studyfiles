23b99d28c8c080a6223e7cd04173c04a5917f1b0
James Wu <justinwpro@gmail.com>
2022-07-05 22:50:29 -0500

    update

--- React_challenge/advanced-todolist/src/App.js
+++ React_challenge/advanced-todolist/src/App.js
@@ -1,99 +1,119 @@
 import React,{useState} from "react";
-import './App.css';
+import "./App.css";
 
-function App() {
+
+
+const App = () => {
   let [value, setValue] = useState("");
-  let [searchValue, setSearchValue] = useState("");
-  let [item, setItem] = useState([]);
+  let [list, setList] = useState([]);
+  let [editValue, setEditValue] = useState("");
+  let [editIndex, setEditIndex] = useState(-1);
   let [order, setOrder] = useState(1);
-  let [input, setInput] = useState("");
-  let [index, setIndex] = useState(-1);
-  let [line, setLine] = useState(false);
+  let [search, setSearch] = useState("");
+  let [sign, setSign] = useState(0);
+  
   const addHandler = () =>{
-      let newItem = {
-        id: Math.floor(Math.random()*1000),
-        content: value,
-        order:order,
-      }
-    setItem((pre) => [...pre, newItem]);
-    setValue("");
+     let newValue={
+       id:Math.floor(Math.random()*1000),
+       content: value,
+       style:false,
+       order:order,
+     }
+     setList((pre) => [...pre, newValue]);
+     setValue("");
   }
+
   const deleteHandler = (id) =>{
-     let newItem = item.filter((one) =>{
-       return one.id !== id;
-     })
-     setItem(newItem);
+     let newList = list.filter((one) => one.id !== id);
+     setList(newList);
   }
 
-  const changeOrder = (abc, order) =>{
-    let newItem1 = {
-      id: abc.id,
-      content: abc.content,
-      order: order,
-    };
-    deleteHandler(abc.id);
-    setItem((pre) => [...pre, newItem1]);
+  const styleHandler = (index) =>{
+    let newList1 = [...list];
+    newList1[index].style = !newList1[index].style;
+    setList(newList1);
   }
 
-  const saveHandler = (one, input) =>{
-    let newItem2 = {
-        id:one.id,
-        content: input,
-        order: one.order,
-    }
-    deleteHandler(one.id);
-    setItem((pre) => [...pre, newItem2]);
-    setInput("");
-    setIndex(-1);
+  const saveHandler = (index, editValue) =>{
+    let newList2 = [...list];
+    newList2[index].content = editValue;
+    setList(newList2);
+    setEditValue("");
+    setEditIndex(-1);
   }
 
   const cancelHandler = () =>{
-    setIndex(-1);
-    setInput("");
+    setEditValue("");
+    setEditIndex(-1);
   }
 
-  const setInputHandler = (e) =>{
-     setInput(e.target.value);
+  const setListOrder = (one,order) =>{
+     let newOne = {
+       id:one.id,
+       content: one.content,
+       style:one.style,
+       order:order,
+     } 
+     deleteHandler(one.id);
+     setList((pre) => [...pre, newOne]);
+  }
+
+  const allHandler = () =>{
+    setSign(0);
+  }
+
+  const processHandler = () =>{
+    setSign(1);
+  }
+
+  const doneHandler = () =>{
+    setSign(2);
   }
 
   return (
-    <div>
-       {item.length !== 0 && <input type="text" placeholder="search the to do list" value={searchValue} onChange={(e) => setSearchValue(e.target.value)}/>}
-       <ul>
-       {
-         item.filter(li => li.content.includes(searchValue))
-         .sort( (a,b) => a.order - b.order)
-         .map((one) => {
-           return (
-               <li key={one.id}><input type="number" 
-               value={one.order} 
-               onChange={(e) => {changeOrder(one, e.target.value)}}/>
-               <button onClick={() => {setIndex(one.id); setInput(one.content)}}>edit</button>
-               <button onClick={() => deleteHandler(one.id)}>delete</button>
-               {index !== one.id ? 
-               <span>{one.content}</span> 
-               : null}
-               {index === one.id ? 
-               (<>
-                  <input type="text" value={input} onChange={(e) => setInputHandler(e)}/>
-                  <button onClick={() => cancelHandler()}>cancel</button>
-                  <button onClick={() => saveHandler(one, input)}>save</button>
+    <>
+    <input type="text" placeholder="add" value={value} onChange={(e) => setValue(e.target.value)} onKeyDown={(e) => e.key==="Enter" ? addHandler() : null}/>
+    <input type="text" placeholder="search" value={search} onChange={(e) => setSearch(e.target.value)}/>
+    <p style={{color: sign === 0 ? "red" : ""}} onClick={() => allHandler()}>All</p>
+    <p style={{color: sign === 1 ? "red" : ""}} onClick={() => processHandler()}>process</p>
+    <p style={{color: sign === 2 ? "red" : ""}} onClick={() => doneHandler()}>done</p>
+    <ul>
+      {
+        list.filter((one) => {
+          return sign === 0 ? one.content.toLowerCase().includes(search.toLowerCase()) 
+              :  sign === 1 ? one.style && one.content.toLowerCase().includes(search.toLowerCase()) 
+              :  sign === 2 ? !one.style && one.content.toLowerCase().includes(search.toLowerCase()) 
+              : ""
+        })
+        .sort((a,b) => a.order - b.order)
+        .map((one, i) =>{
+          return (
+            <li key={one.id}>
+              <input type="number" value={one.order} onChange={(e) => {setListOrder(one, e.target.value);setOrder(e.target.value)}} />
+            {editIndex !== one.id ? 
+            (
+              <span style={{textDecoration : one.style ? "line-through" : ""}} onClick={() => styleHandler(i)}>{one.content}</span>
+            ): null 
+          }
+            {editIndex === one.id ? 
+              (
+                <>
+                 <input type="text" value={editValue} onChange={(e) => setEditValue(e.target.value)}/>
+                 <button onClick={() => saveHandler(i, editValue)}>save</button>
+                 <button onClick = {() => cancelHandler()}>cancel</button>
                 </>
-                ):undefined
-               }
-               </li>
-           )
-         })
-       }
-       </ul>
-       {!item.length && <h2>No todos here...</h2>}
-       <input type="text" placeholder = "come on!!"
-       value={value} 
-       onChange={(e) => setValue(e.target.value)} 
-       onKeyDown={(e)=> e.key === "Enter" ? addHandler() : null}/>
+              ):null
+            }
+            <button disabled = {one.style} onClick={() => {setEditIndex(one.id); setEditValue(one.content)}}>edit</button>
+            <button onClick={() => deleteHandler(one.id)}>delete</button>
+            </li>
+          )
+        })
+      }
+    </ul>
 
-    </div>
+    </>
   );
-}
+};
 
 export default App;
--- React_practice/practice_1/src/App.js
+++ React_practice/practice_1/src/App.js
@@ -6,56 +6,112 @@ import "./App.css";
 const App = () => {
   let [value, setValue] = useState("");
   let [list, setList] = useState([]);
-  let [updateValue, setUpdateValue] = useState("");
-  let [showId, setShowId] = useState(-1);
+  let [editValue, setEditValue] = useState("");
+  let [editIndex, setEditIndex] = useState(-1);
+  let [order, setOrder] = useState(1);
+  let [search, setSearch] = useState("");
+  let [sign, setSign] = useState(0);
+  
   const addHandler = () =>{
-    let newValue = {
-      id: Math.floor(Math.random()*1000),
-      content:value,
-    }
-    setList((pre) => [...pre, newValue]);
-    setValue("");
-  }
-  const deleteHandler = (id) =>{
-      let newList = list.filter((one) => one.id !== id);
-      setList(newList);
-  }
-  const updateHandler = (id, updateValue) =>{
-    let newOne1 = {
-        id:id,
-        content: updateValue,
-      }
-      deleteHandler(id);
-      setList((pre) => [...pre, newOne1]);
-      setUpdateValue("");
-      setShowId(-1);
+     let newValue={
+       id:Math.floor(Math.random()*1000),
+       content: value,
+       style:false,
+       order:order,
+     }
+     setList((pre) => [...pre, newValue]);
+     setValue("");
   }
 
+  const deleteHandler = (id) =>{
+     let newList = list.filter((one) => one.id !== id);
+     setList(newList);
+  }
+
+  const styleHandler = (index) =>{
+    let newList1 = [...list];
+    newList1[index].style = !newList1[index].style;
+    setList(newList1);
+  }
+
+  const saveHandler = (index, editValue) =>{
+    let newList2 = [...list];
+    newList2[index].content = editValue;
+    setList(newList2);
+    setEditValue("");
+    setEditIndex(-1);
+  }
+
+  const cancelHandler = () =>{
+    setEditValue("");
+    setEditIndex(-1);
+  }
+
+  const setListOrder = (one,order) =>{
+     let newOne = {
+       id:one.id,
+       content: one.content,
+       style:one.style,
+       order:order,
+     } 
+     deleteHandler(one.id);
+     setList((pre) => [...pre, newOne]);
+  }
+
+  const allHandler = () =>{
+    setSign(0);
+  }
+
+  const processHandler = () =>{
+    setSign(1);
+  }
+
+  const doneHandler = () =>{
+    setSign(2);
+  }
 
   return (
     <>
-    <input type="text" value={value} 
-    onChange={(e) => setValue(e.target.value)} 
-    onKeyDown= {(e) => e.key === "Enter" ? addHandler() : null}/>
-    <button onClick={() => addHandler()} >add</button>
+    <input type="text" placeholder="add" value={value} onChange={(e) => setValue(e.target.value)} onKeyDown={(e) => e.key==="Enter" ? addHandler() : null}/>
+    <input type="text" placeholder="search" value={search} onChange={(e) => setSearch(e.target.value)}/>
+    <p style={{color: sign === 0 ? "red" : ""}} onClick={() => allHandler()}>All</p>
+    <p style={{color: sign === 1 ? "red" : ""}} onClick={() => processHandler()}>process</p>
+    <p style={{color: sign === 2 ? "red" : ""}} onClick={() => doneHandler()}>done</p>
     <ul>
       {
-        list.map((one) => {
-          return(
-            <li key={one.id}>{one.content}
-            <button onClick={() => {setShowId(one.id)}}>edit</button>
+        list.filter((one) => {
+          return sign === 0 ? one.content.toLowerCase().includes(search.toLowerCase()) 
+              :  sign === 1 ? one.style && one.content.toLowerCase().includes(search.toLowerCase()) 
+              :  sign === 2 ? !one.style && one.content.toLowerCase().includes(search.toLowerCase()) 
+              : ""
+        })
+        .sort((a,b) => a.order - b.order)
+        .map((one, i) =>{
+          return (
+            <li key={one.id}>
+              <input type="number" value={one.order} onChange={(e) => {setListOrder(one, e.target.value);setOrder(e.target.value)}} />
+            {editIndex !== one.id ? 
+            (
+              <span style={{textDecoration : one.style ? "line-through" : ""}} onClick={() => styleHandler(i)}>{one.content}</span>
+            ): null 
+          }
+            {editIndex === one.id ? 
+              (
+                <>
+                 <input type="text" value={editValue} onChange={(e) => setEditValue(e.target.value)}/>
+                 <button onClick={() => saveHandler(i, editValue)}>save</button>
+                 <button onClick = {() => cancelHandler()}>cancel</button>
+                </>
+              ):null
+            }
+            <button disabled = {one.style} onClick={() => {setEditIndex(one.id); setEditValue(one.content)}}>edit</button>
             <button onClick={() => deleteHandler(one.id)}>delete</button>
-            {showId === one.id ? (
-              <span>
-                <input type="text" value={updateValue} onChange={(e) => setUpdateValue(e.target.value)}/>
-                <button onClick={() => updateHandler(one.id, updateValue)}>update</button>
-              </span>
-            ): null}
             </li>
           )
         })
       }
     </ul>
+
     </>
   );
 };
