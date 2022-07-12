5ac3a2208fc3852f27ff4ad8dbb2202902513320
James Wu <justinwpro@gmail.com>
2022-07-12 09:57:27 -0500

    update

--- play.js
+++ play.js
@@ -1,23 +1,39 @@
 let arr = [1, 2, 3, 4, 5];
 
-function myMap(arr, myFunc) {
+Array.prototype.myMap = function (myFunc) {
   const myArr = [];
   for (let i = 0; i < arr.length; i++) {
     const result = myFunc(arr[i], i, arr);
     myArr.push(result);
   }
   return myArr;
-}
+};
 
-console.log(myMap(arr, (num) => num + 2));
+console.log(arr.myMap((x) => x * 2));
 
-function myMap(arr, myFunc) {
+Array.prototype.myFilter = function (myFunc) {
   const myArr = [];
   for (let i = 0; i < arr.length; i++) {
     const result = myFunc(arr[i], i, arr);
     if (result) {
-      myArr.push(result);
+      myArr.push(arr[i]);
     }
   }
   return myArr;
+};
+
+console.log(arr.myFilter((x) => x % 2 === 0));
+
+function flatten(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      result = result.concat(flatten(arr[i]));
+    } else {
+      result.push(arr[i]);
+    }
+  }
+  return result;
 }
+
+console.log(flatten([1, 2, 3, [2, 3, [5, 6]]]));
