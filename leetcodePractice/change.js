b0dfc4729e8d8307bf1a002e5821f50f72b103bb
James Wu <justinwpro@gmail.com>
2022-07-25 23:20:33 -0500

    update

--- leetcodePractice/change.js
+++ leetcodePractice/change.js
@@ -3,14 +3,45 @@ const coins = [2,3,7];
 
 
 const coin = (target) =>{
-    let dp = Array(target + 1).fill(Infinity);
-    dp[0] = 0;
-    for(let coin of coins){
-        for(let i = coin; i<=target; i++){
-            dp[i] = Math.min(dp[i], dp[i-coin] + 1);
-        }
-    }
-    return dp[target];
+ 
+    
 }
 
 console.log(coin(23));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const coin = (target) =>{
+//     let dp = Array(target + 1).fill(Infinity);
+//     dp[0] = 0;
+//     for(let coin of coins){
+//         for(let i = coin; i<=target; i++){
+//             dp[i] = Math.min(dp[i], dp[i-coin] + 1);
+//         }
+//     }
+//     return dp[target];
+// }
+
+// console.log(coin(23));
--- leetcodePractice/climbing.js
+++ leetcodePractice/climbing.js
@@ -1,12 +1,49 @@
 const climbing = (n) =>{
-   let dp = [];
-   dp[1] = 1;
-   dp[2] = 2;
-   for(let i = 3; i<=n;i++){
-       dp[i] = dp[i-1] + dp[i-2];
-   }
-   return dp[n];
+
+    
 }
 
 console.log(climbing(5));
 console.log(climbing(3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const climbing = (n) =>{
+//    let dp = [];
+//    dp[1] = 1;
+//    dp[2] = 2;
+//    for(let i = 3; i<=n;i++){
+//        dp[i] = dp[i-1] + dp[i-2];
+//    }
+//    return dp[n];
+// }
+
+// console.log(climbing(5));
+// console.log(climbing(3));
