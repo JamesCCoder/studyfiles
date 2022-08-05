74b640ff77e1a3d72a3fde14ad3cf86c135af193
James Wu <justinwpro@gmail.com>
2022-08-04 23:49:01 -0500

    update

--- Challenge/Component/shape/src/App.js
+++ Challenge/Component/shape/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import './App.css';
 
 import Triangle from "./component/Triangle";
+import Zoomhover from "./component/Zoomhover";
 
 function App() {
   return (
     <div>
        <Triangle />
+       <Zoomhover />
     </div>
   );
 }
---
+++ Challenge/Component/shape/src/component/Zoomhover.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Zoomhover.css";
+
+const Zoomhover = () => {
+    return ( 
+        <>
+        <div className="square"></div>
+        <div className="square1">fsdf</div>
+        <div className="square2"></div>        
+        </>
+
+     );
+}
+ 
+export default Zoomhover;
