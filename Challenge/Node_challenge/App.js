a442b5e4e448fe3948b23d24b145af0daaff3619
James Wu <justinwpro@gmail.com>
2022-08-17 16:17:52 -0500

    upfate

--- Challenge/Node_challenge/App.js
+++ Challenge/Node_challenge/App.js
@@ -1,54 +1,30 @@
+const http = require("http");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const http = require("http");
-
-// const server = http.createServer((req, res) => {
-//     const url = req.url;
+const server = http.createServer((req, res) => {
+    const url = req.url;
      
-//     if(url === '/') {
-//         res.write('<html>');
-//         res.write('<p>GeeksforGeeks</p>');
-//         res.write('</html>');
-//         return res.end();
-//     }
+    if(url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<html>');
+        res.write('<p>GeeksforGeeks</p>');
+        res.write('</html>');
+        return res.end();
+    }
      
-//     if(url === '/about') {
-//         res.write('<html>');
-//         res.write('<p>GeeksforGeeks- Node.js</p>');
-//         res.write('</html>');
-//         return res.end();
-//     }
-// });
+    if(url === '/about') {
+        res.write('<html>');
+        res.write('<p>GeeksforGeeks- Node.js</p>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if(url === "*"){
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.write('404 not found');
+        return res.end();
+    }
+});
  
-// server.listen(3000, () => {
-//     console.log("Server listening on port 3000")
-// });
+server.listen(3000, () => {
+    console.log("Server listening on port 3000")
+});
--- play.js
+++ play.js
@@ -1,16 +1,1 @@
-let arr = [1,2,3,2,3];
-
-const duplicate = (arr) =>{
-
-    let res = [];
-    let mySet = new Set();
-    for(let i = 0; i<arr.length; i++){
-        if(mySet.has(arr[i])){
-            res.push(arr[i]);
-        }
-        mySet.add(arr[i]);
-    }
-    return res;
-}
-
-console.log(duplicate(arr));
+console.log(Number({}));
