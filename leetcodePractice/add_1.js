17632523dc6030274f05631739b7c91fb10eb17b
James Wu <justinwpro@gmail.com>
2022-08-19 19:07:40 -0500

    update

---
+++ leetcodePractice/add_1.js
@@ -0,0 +1,77 @@
+const str = "addBoxHere0123---";
+
+const add = (str) =>{
+
+    
+}
+
+console.log(add(str));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const add = (str) =>{
+//    let arr = str.split("");
+//    let exp = /[a-zA-Z0-9-]/;
+//    for(let i = 0; i<arr.length; i++){
+//        if(exp.test(arr[i])){
+//            arr.splice(i, 1, "_", arr[i]);
+//            i++;
+//        }
+//    }
+//    return arr.join("");
+// }
+
+// console.log(add(str));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ leetcodePractice/add_2.js
@@ -0,0 +1,50 @@
+const str = "123456123";
+
+const add = (str) =>{
+
+    
+}
+
+console.log(add(str));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const str = "123456123";
+
+// const add = (str) =>{
+//     let arr = str.split("");
+//     let mySet = new Set();
+//     for(let i = 0; i<arr.length; i++){
+//         if(mySet.has(arr[i])){
+//             arr.splice(i, 1, "_", arr[i]);
+//             i++;
+//         }
+//         mySet.add(arr[i]);
+//     }
+//     return arr;
+// }
+
+// console.log(add(str));
