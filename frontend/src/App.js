5e6f51475dd6edc376ec740c2945b53f74af4fcf
James Wu <justinwpro@gmail.com>
2022-09-15 23:30:46 -0500

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,56 @@
-import React,{useState, useRef,useEffect } from "react";
-import "./App.css";
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { actions } from "./store/index.js";
+import { dataFetch } from "./store/index.js";
 
 const App = () => {
-  const [show, setShow] = useState(false);
-  const myRef = useRef();
-  const editHandler = () =>{
-    if(myRef.current){
-      myRef.current.focus();
+  const value = useSelector(state => state.value);
+  const list = useSelector(state => state.list);
+  const isLoading = useSelector(state => state.isLoading);
+  const dispatch = useDispatch();
+
+  const addHandler = () =>{
+    dispatch(actions.addValue(1));
+  }
+  const minusHandler = () =>{
+    dispatch(actions.addValue(-1));
+  }
+  const oddHandler = () =>{
+    if(value % 2 !== 0){
+       dispatch(actions.addValue(1));
     }
   }
+  const asyncHandler = () =>{
+    let timeout = setTimeout(() => {
+       dispatch(actions.addValue(1));
+    }, 1000);
+    return () => clearTimeout(timeout);
+  }
 
+  const dataHandler = () =>{
+     dispatch(dataFetch());
+  }
   return ( 
     <div>
-      {show && <input ref = {myRef}/>}
-      <button onClick={editHandler}>edit</button>
+       <div>{value}
+        <button onClick={addHandler}>add one</button>
+        <button onClick={minusHandler}>minute one</button>
+        <button onClick={oddHandler}>add one when odd</button>
+        <button onClick={asyncHandler}>add one after one sec</button>
+       </div>
+    <div>
+       <button onClick={dataHandler}>getData</button>
+       {isLoading && <p>loading</p>}
+       {
+          list.map((one,i) =>{
+            return(
+              <p key={i}>{one.title}</p>
+            )  
+          })
+       }
     </div>
+    </div>
+
    );
 }
  
---
+++ frontend/src/store/index.js
@@ -0,0 +1,40 @@
+import {createSlice, configureStore, createAsyncThunk} from "@reduxjs/toolkit";
+
+export const dataFetch = createAsyncThunk(
+    "data/getData",
+    async () =>{
+        const data = await fetch("http://jsonplaceholder.typicode.com/posts");
+        const dataFormat = await data.json();
+        return dataFormat;
+    }
+)
+
+const dataSlice = createSlice({
+    name:"data",
+    initialState:{value:0, isLoading:false, list:[]},
+    reducers:{
+        addValue(state,action){
+            state.value = state.value + action.payload;
+        }
+    },
+    extraReducers:{
+        [dataFetch.pending]:(state,action) =>{
+            state.isLoading = true;
+        },
+        [dataFetch.fulfilled]:(state,action) =>{
+            state.isLoading = false;
+            state.list = action.payload;
+        },
+        [dataFetch.rejected]:(state,action) =>{
+            state.isLoading = false;
+        }
+    },
+})
+
+export const actions = dataSlice.actions;
+
+const store = configureStore({
+    reducer:dataSlice.reducer,
+})
+
+export default store;
