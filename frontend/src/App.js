6b03d60c2dfdee035d8763c9d60766387ccd4db4
James Wu <justinwpro@gmail.com>
2022-09-18 00:23:17 -0500

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,56 +1,22 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { actions } from "./store/index.js";
-import { dataFetch } from "./store/index.js";
+import React, { useState, useRef, useEffect } from "react";
 
 const App = () => {
-  const value = useSelector(state => state.value);
-  const list = useSelector(state => state.list);
-  const isLoading = useSelector(state => state.isLoading);
-  const dispatch = useDispatch();
-
-  const addHandler = () =>{
-    dispatch(actions.addValue(1));
+  const [show, setShow] = useState(false);
+  const myRef = useRef();
+  const editHandler = () =>{
+     setShow(pre => !pre)
   }
-  const minusHandler = () =>{
-    dispatch(actions.addValue(-1));
-  }
-  const oddHandler = () =>{
-    if(value % 2 !== 0){
-       dispatch(actions.addValue(1));
+  useEffect(() =>{
+    if(show){
+      myRef.current.focus();
     }
-  }
-  const asyncHandler = () =>{
-    let timeout = setTimeout(() => {
-       dispatch(actions.addValue(1));
-    }, 1000);
-    return () => clearTimeout(timeout);
-  }
+  },[show])
 
-  const dataHandler = () =>{
-     dispatch(dataFetch());
-  }
   return ( 
     <div>
-       <div>{value}
-        <button onClick={addHandler}>add one</button>
-        <button onClick={minusHandler}>minute one</button>
-        <button onClick={oddHandler}>add one when odd</button>
-        <button onClick={asyncHandler}>add one after one sec</button>
-       </div>
-    <div>
-       <button onClick={dataHandler}>getData</button>
-       {isLoading && <p>loading</p>}
-       {
-          list.map((one,i) =>{
-            return(
-              <p key={i}>{one.title}</p>
-            )  
-          })
-       }
+      {show && <input ref={myRef}/>}
+      <button onClick={editHandler}>edit</button>
     </div>
-    </div>
-
    );
 }
  
