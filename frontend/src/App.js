f99d48657ea10e92deebb694115b6d227a6f36b9
James Wu <justinwpro@gmail.com>
2022-09-26 00:23:25 -0500

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,34 @@
-import React,{useRef} from "react";
+import React,{useState} from "react";
 import "./App.css";
-import { useSelector, useDispatch } from "react-redux";
-import { actions } from "./store/index.js";
-
 
 const App = () => {
-  const text = useSelector(state => state.text);
-  const num = useSelector(state => state.num);
-  const value = useSelector(state => state.value);
-  const list = useSelector(state => state.list);
-  const dispatch = useDispatch();
-  const myRef = useRef(null);
-
-  const changeHandler = () =>{
-      myRef.current = setInterval(() => {
-        dispatch(actions.addValue(1));
-      }, 1000);
-  }
-  const stopHandler = () =>{
-      clearInterval(myRef.current);
-      myRef.current = null;
-  }
-
+  const [value, setValue] = useState("");
+  const [list, setList] = useState([]);
   const addHandler = () =>{
-     dispatch(actions.changeList(value));
-     dispatch(actions.changeValue([text, num]));
-     
+    setList(pre => [...pre, value]);
+    setValue("");
+  }
+
+  const deleteHandler = (index) =>{
+     let newList = [...list];
+     newList.splice(index, 1);
+     setList(newList);
   }
   return ( 
     <div>
-       <input type="text" value={text} onChange={e => dispatch(actions.changeText(e.target.value))}/>
-       <input type="text" value={num} />
-       <button onClick={changeHandler}>change</button>
-       <button onClick={stopHandler}>stop</button>
-       <button onClick={addHandler}>add</button>
-       {
-         list.map((one,i) =>{
-           return <p key={i}>{one}</p>
-         })
-       }
+      <input type="string" value={value} onChange={e => setValue(e.target.value)}/>
+      <button onClick={addHandler}>add</button>
+      <ul>
+        {
+          list.map((one, i) =>{
+            return(
+              <li key={i}>{one}
+              <button onClick={() => deleteHandler(i)}>delete</button>
+              </li>
+            )
+          })
+        }
+      </ul>
     </div>
    );
 }
